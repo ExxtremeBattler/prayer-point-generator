e65d5165976c82beabe23b5c61f34bf7249d0695
ExxtremeBattler <69109391+ExxtremeBattler@users.noreply.github.com>
2023-04-10 12:51:54 +0100

    added component scaffolding

---
+++ src/components/generateSection.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+function generateSection() {
+    return(
+        <div class="card">
+        <div class="card-header">
+          <h2>Generate a Password</h2>
+        </div>
+        <textarea
+            readonly
+            id="password"
+            placeholder="Your Secure Password"
+            aria-label="Generated Password"
+          ></textarea>
+          <button onclick="getPasswordOptions()" id="generate" className="btn"> Generate Password</button>
+        </div>
+    )
+}
+
+export default generateSection
---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Header () {
+    return(
+        <header>
+            <h1> Prayer Generator </h1>
+        </header>
+    )
+}
+
+export default Header
