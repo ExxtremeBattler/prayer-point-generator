0cfb4d443a895479b47986b0e1d6e4ee23f53695
ExxtremeBattler <69109391+ExxtremeBattler@users.noreply.github.com>
2023-04-13 15:41:25 +0100

    turned scripture into state, instead of variable

--- src/components/generateSection.js
+++ src/components/generateSection.js
@@ -5,22 +5,30 @@ import axios from "axios";
 
 const BASE_URL = "https://bible-api.com/john3:16"
 
-let topicVerse = ""
+
+const topic = (prayerTopics[Math.floor(Math.random() * prayerTopics.length)])
 
 
+// function V(verseTopic) {
+//   if (verseTopic === "Thanksgiving") {
 
-  const getVerse = async () =>{
-    const resp = await axios.get(`${BASE_URL}`)
-    topicVerse = resp.data.text
-    console.log(topicVerse);
-  }
+//   } else {
+   
+//   }
+// }
 
-
-getVerse()
 
 function GenerateSection() {
 
-  const [prayerTopic, setPrayerTopic] = useState(prayerTopics[Math.floor(Math.random() * prayerTopics.length)])
+  const [topicVerse, setTopicVerse] = useState("")
+
+  const getVerse = async () =>{
+    const resp = await axios.get(`${BASE_URL}`)
+    setTopicVerse(resp.data.text)
+    console.log(topicVerse);
+  }
+
+  getVerse()
 
 
     return(
@@ -31,8 +39,16 @@ function GenerateSection() {
         <textarea
             readonly
             id="password"
-            value={prayerTopic}
+            value={topic}
             placeholder="Your Prayer"
+            aria-label="Generated Password"
+          ></textarea>
+
+        <textarea
+            readonly
+            id="password"
+            value={topicVerse}
+            placeholder="Your Scriptures"
             aria-label="Generated Password"
           ></textarea>
         </div>
