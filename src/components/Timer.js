576d77b54af4a757c4cc6bc38e9010aff660b027
ExxtremeBattler <69109391+ExxtremeBattler@users.noreply.github.com>
2023-05-18 14:58:17 +0100

    created timer component, installed npm circl timer

---
+++ src/components/Timer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Timer = () => {
+    const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  return (
+    <div className="timer">
+    </div>
+  );
+};
+
+export default Timer;
